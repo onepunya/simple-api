2ce26134b5f5859c40887d444597faba23cdf390
ArifzynXD <arifzyn906@gmail.com>
2023-12-12 22:59:06 +0700

    api

---
+++ system/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDefinitions = require('./swagger/swaggerDefinition');
+const routes = require('./routes');
+
+const app = express();
+const port = 3000;
+
+const swaggerOptions = {
+  swaggerDefinition: swaggerDefinitions,
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
